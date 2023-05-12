407ce575e449a131b1af355aae561b5ed3762eed
Vincent Riva <vincent.riva@outlook.fr>
2023-05-12 17:57:31 +0200

    ✨ Add base blueprints

--- index.php
+++ index.php
@@ -4,4 +4,12 @@
 
 use Kirby\Cms\App as Kirby;
 
-Kirby::plugin('treast/cookieconsent', []);
+Kirby::plugin('treast/cookieconsent', [
+    'blueprints' => [
+        'cookieconsent' => __DIR__ . '/blueprints/cookies.yml',
+        'fields/cookies/config' => __DIR__ . '/blueprints/fields/config.yml',
+        'fields/cookies/cookies' => __DIR__ . '/blueprints/fields/cookies.yml',
+        'fields/cookies/consent' => __DIR__ . '/blueprints/fields/consent.yml',
+        'fields/cookies/settings' => __DIR__ . '/blueprints/fields/settings.yml',
+    ]
+]);
